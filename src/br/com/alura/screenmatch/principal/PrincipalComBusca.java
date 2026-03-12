a0b1c821a671e58ff382e6d478765575006fc808
Matheus Antero <101403401+Anterosuehtam@users.noreply.github.com>
2026-03-12 12:54:22 -0300

    busca um filme dinamicamente

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,13 +5,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
         HttpClient client = HttpClient.newHttpClient();
 
+        System.out.println("Digite um filme para buscar as informações: ");
+        var busca = scanner.nextLine();
+
+        var endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=cfb6762e";
+
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=cfb6762e"))
+                .uri(URI.create(endereco))
                 .build();
 
         HttpResponse<String> response = client
